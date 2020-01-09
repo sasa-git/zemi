9fb7702d6bfcecc322ee17c134247ef17439cb96
sasasoni <sasasoni1119@gmail.com>
2020-01-09 14:41:52 +0900

    add opencv test

---
+++ camera/camera-test.py
@@ -0,0 +1,18 @@
+import picamera
+import picamera.array
+import cv2
+
+with picamera.PiCamera() as camera:
+    with picamera.array.PiRGBArray(camera) as stream:
+        camera.resolution = (320,240)
+        
+        while True:
+            camera.capture(stream, 'bgr', use_video_port=True)
+            cv2.imshow('frame', stream.array)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            stream.seek(0)
+            stream.truncate()
+
+        cv2.destroyAllWindows()
